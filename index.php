7bf7850aab423a0b1405ba82c194e0b3273d16b5
mydao <mdtranhuu@gmail.com>
2015-07-17 10:52:37 +0200

    Add contents and some headers

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <!doctype html>
-<html lang="en">
+<html lang="fr">
 <head>
 	<meta charset="UTF-8">
-	<title>Document</title>
+	<title>Brocoli</title>
 </head>
 <body>
-	
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati, iure consequatur ullam veritatis nulla cumque fugiat dolore. Repellendus, sunt atque explicabo tempore facere corporis repellat expedita recusandae voluptatem. Sed, id.</p>
 </body>
 </html>
