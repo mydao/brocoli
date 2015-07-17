4458ad7c16976069967196531aa043402d0803ca
mydao <mdtranhuu@gmail.com>
2015-07-17 11:09:00 +0200

    add main stylesheet

--- index.php
+++ index.php
@@ -3,8 +3,11 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Brocoli</title>
+	<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+
 	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati, iure consequatur ullam veritatis nulla cumque fugiat dolore. Repellendus, sunt atque explicabo tempore facere corporis repellat expedita recusandae voluptatem. Sed, id.</p>
+	
 </body>
 </html>
